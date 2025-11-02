cc6ac380d276f66488f1e780f7de31d7e5166757
muntasimfuad <muntasimfuad@protonmail.com>
2025-11-02 18:52:52 +0600

    Update deprecated ggplot2 size parameter to linewidth
    - Replace element_line(size = ...) with element_line(linewidth = ...)
    - Replace geom_line(size = ...) with geom_line(linewidth = ...)
    - Fixes ggplot2 3.4.0+ deprecation warnings
    - Addresses: 'The size argument of element_line() is deprecated'

--- R/plot_rem.R
+++ R/plot_rem.R
@@ -41,8 +41,8 @@ plot_rem <- function(meta_diffexp, jobname, outputfolder, genecol, metathr) {
         theme_classic() +
 	theme(panel.border= element_blank()) +
 	theme(axis.text.x = element_text(angle = 0, vjust = 0.5)) +
-	theme(axis.line.x = element_line(color = "black", size = 0.6, 
+	theme(axis.line.x = element_line(color = "black", linewidth = 0.6, 
 					 lineend = "square"),
-	      axis.line.y = element_line(color = "black", size = 0.6, 
+	      axis.line.y = element_line(color = "black", linewidth = 0.6, 
 					 lineend = "square"))
 }
